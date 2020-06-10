09024e122fec9f5ecec9963fb260d9198aa9beeb
Shepa <tsef47@mail.ru>
2020-06-10 17:18:56 +0300

    [+] Added TMPro [+] Added main scene and stupid logic for notes

---
+++ Assets/Scripts/MainNotesLogic.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainNotesLogic : MonoBehaviour
+{
+    [Header("Buttons in Main Menu")]
+    public Button clearNotes;
+    public Button saveNotes;
+    [Header("Text Area for write")]
+    public TMP_InputField InputFieldforNotes;
+
+    public TMP_Text infoTitle;
+
+    private int number = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ViewInfo("Hi bitch"));
+  
+        clearNotes.onClick.AddListener(KillAmAll);
+        saveNotes.onClick.AddListener(GodsSave);
+        
+        InputFieldforNotes.text = AllSeeing();
+    }
+
+    private void KillAmAll()
+    {
+        PlayerPrefs.DeleteAll();
+        InputFieldforNotes.text = AllSeeing();
+        StartCoroutine(ViewInfo("I kill all"));
+    }
+
+    private void GodsSave()
+    {
+        PlayerPrefs.SetString("Notes" + number, InputFieldforNotes.text);
+        StartCoroutine(ViewInfo("I save all your shit"));
+    }
+
+    private string AllSeeing()
+    {         
+        return PlayerPrefs.GetString("Notes" + number, "_");
+    }
+
+    private IEnumerator ViewInfo(string info)
+    {
+        infoTitle.text = info;
+        yield return new WaitForSeconds(2f);
+        infoTitle.text = "";
+    }
+
+}
